174219d9ce5b42bea939b667be12eb7ccb841e94
L1fe0nMars <jkama005@gmail.com>
2023-11-19 01:28:27 -0500

    Added new project

--- Portfolio/home.js
+++ Portfolio/home.js
@@ -12,6 +12,90 @@ const JAVA_LINK = document.getElementById("java-link");
 
 const projects = [
     {
+        title: "Blackjack",
+        features: [
+            "Play the Classic Casino Game",
+            "Place Bets, Hit, Stand, and Play Against the Dealer",
+            "Persistent Stats",
+        ],
+        description: `Blackjack is one of the most popular casino card games ever. It's an extremely simple game, but also one that's entirely based on chance. I'm no gambler, 
+            but I do enjoy a good card game. I thought it would be the perfect project to test myself at how well I can implement various game systems and elements. There 
+            were a lot of moving parts to consider.<br><br>
+            
+            Object-oriented programming was a must to complete this project. The cards, deck, hands, player, and dealer are all objects. This approach makes the most sense 
+            when dealing with game elements that need to interact with one another. The game has your typical actions that the player can choose. They must first place a 
+            bet using imaginary money, and then decide whether they want to hit or stand. In addition to the game itself, I thought it would be a cool idea to keep track 
+            of different stats, such as wins, losses, draws, biggest payout, and more. These stats are saved to the browser's local storage and can be accessed anytime. 
+            All of these features and a simple UI make for an engaging web project to play.`,
+        link: "/Blackjack/index.html",
+        linkText: "Visit Project",
+        logos: {
+            "html": "block",
+            "css": "block",
+            "javascript": "block",
+            "react": "none",
+            "swiperjs": "none",
+        },
+        sourceCode: "https://github.com/L1fe0nMars/ProjectPortfolio/tree/main/Blackjack",
+        javaBtnDisplay: "block",
+        javaSourceLink: "https://github.com/L1fe0nMars/Blackjack",
+    },
+    {
+        title: "WeatherGuesser",
+        features: [
+            "Play a Unique Weather Guessing Game",
+            `Gathers Weather Data Using <a href="https://openweathermap.org/" target="_blank" rel="noopener noreferrer" class="text-link">OpenWeatherMap API</a>`,
+            "Settings to Customize the Game to Your Preferences",
+        ],
+        description: `Everyone and their mothers has already built a weather app before, so I wanted to put my own twist to it. Introducing WeatherGuesser, a weather 
+            guessing game that puts your geography and weather knowledge to the test. The game is simple, it gives you a random city form anywhere in the world, some
+            weather info such as the humidity, and you have to guess what the current temperature is in that city.<br><br>
+            
+            Using React made it easy to handle various aspects of WeatherGuesser. Organization is a big part of the way I code, and being able to split everything into
+            components keeps the project neat and maintainable. It also allows me to easily expand on the game if I choose to do so. Another big aspect of why I went with React
+            is how effortless it is to handle save data. WeatherGuesser features settings to adjust the game to your liking and React makes it seamless to handle the states of 
+            the saved data. All these aspects come together harmoniously to give a fun and interactive game.`,
+        link: "https://master--keen-torte-d6dd9d.netlify.app/",
+        linkText: "Visit Project",
+        logos: {
+            "html": "none",
+            "css": "none",
+            "javascript": "none",
+            "react": "block",
+            "swiperjs": "none",
+        },
+        sourceCode: "https://github.com/L1fe0nMars/weatherguesser",
+        javaBtnDisplay: "none",
+        javaSourceLink: "",
+    },
+    {
+        title: "21 Classics",
+        features: [
+            "Responsive Design",
+            "Light/Dark Mode Toggle",
+            `Swipeable Images Using <a href="https://swiperjs.com/" target="_blank" rel="noopener noreferrer" class="text-link">SwiperJS</a> Library`,
+        ],
+        description: `21 Classics is a website dedicated to my dad's collector car business. Originally created by myself with a website builder in 2020, 
+            the website underwent a complete overhaul in July 2023 as I began my journey into front-end development. It was remade entirely from scratch using pure HTML, 
+            CSS, and JavaScript. The goal was to make the design simple but elegant, while making sure the cars stand out.<br><br>
+            
+            UI/UX were key in my design choices. The website's responsive design allows the content to adapt to any screen size, while features such as the light/dark mode toggle 
+            and swipeable images let the user have an interactive experience when viewing the content. Buttons were specifically chosen to be colorful to make them stand out, and 
+            the simple header makes it easy for the user to navigate through the site. The 21 Classics website is a perfect example of how there's beauty in simplicity.`,
+        link: "https://21classics.com/",
+        linkText: "Visit Website",
+        logos: {
+            "html": "block",
+            "css": "block",
+            "javascript": "block",
+            "react": "none",
+            "swiperjs": "block",
+        },
+        sourceCode: "https://github.com/L1fe0nMars/21Classics",
+        javaBtnDisplay: "none",
+        javaSourceLink: "",
+    },
+    {
         title: "Planet Name Generator",
         features: [
             "Generate Billions of Unique Names",
@@ -31,65 +115,12 @@ const projects = [
             "html": "block",
             "css": "block",
             "javascript": "block",
+            "react": "none",
             "swiperjs": "none",
         },
         sourceCode: "https://github.com/L1fe0nMars/ProjectPortfolio/tree/main/PlanetNameGenerator",
         javaBtnDisplay: "block",
         javaSourceLink: "https://github.com/L1fe0nMars/PlanetNameGenerator",
-    },
-    {
-        title: "21 Classics",
-        features: [
-            "Responsive Design",
-            "Light/Dark Mode Toggle",
-            "Swipeable Images Using SwiperJS API",
-        ],
-        description: `21 Classics is a website dedicated to my dad's collector car business. Originally created by myself with a website builder in 2020, 
-            the website underwent a complete overhaul in July 2023 as I began my journey into front-end development. It was remade entirely from scratch using pure HTML, 
-            CSS, and JavaScript. The goal was to make the design simple but elegant, while making sure the cars stand out.<br><br>
-            
-            UI/UX were key in my design choices. The website's responsive design allows the content to adapt to any screen size, while features such as the light/dark mode toggle 
-            and swipeable images let the user have an interactive experience when viewing the content. Buttons were specifically chosen to be colorful to make them stand out, and 
-            the simple header makes it easy for the user to navigate through the site. The 21 Classics website is a perfect example of how there's beauty in simplicity.`,
-        link: "https://21classics.com/",
-        linkText: "Visit Website",
-        logos: {
-            "html": "block",
-            "css": "block",
-            "javascript": "block",
-            "swiperjs": "block",
-        },
-        sourceCode: "https://github.com/L1fe0nMars/21Classics",
-        javaBtnDisplay: "none",
-        javaSourceLink: "",
-    },
-    {
-        title: "Blackjack",
-        features: [
-            "Play the Classic Casino Game",
-            "Place Bets, Hit, Stand, and Play Against the Dealer",
-            "Persistent Stats",
-        ],
-        description: `Blackjack is one of the most popular casino card games ever. It's an extremely simple game, but also one that's entirely based on chance. I'm no gambler, 
-            but I do enjoy a good card game. I thought it would be the perfect project to test myself at how well I can implement various game systems and elements. There 
-            were a lot of moving parts to consider.<br><br>
-            
-            Object-oriented programming was a must to complete this project. The cards, deck, hands, player, and dealer are all objects. This approach makes the most sense 
-            when dealing with game elements that need to interact with one another. The game has your typical actions that the player can choose. They must first place a 
-            bet using imaginary money, and then decide whether they want to hit or stand. In addition to the game itself, I thought it would be a cool idea to keep track 
-            of different stats, such as wins, losses, draws, biggest payout, and more. These stats are saved to the browser's local storage and can be accessed anytime. 
-            All of these features and a simple UI make for an engaging web application to play.`,
-        link: "/Blackjack/index.html",
-        linkText: "Visit Project",
-        logos: {
-            "html": "block",
-            "css": "block",
-            "javascript": "block",
-            "swiperjs": "none",
-        },
-        sourceCode: "https://github.com/L1fe0nMars/ProjectPortfolio/tree/main/Blackjack",
-        javaBtnDisplay: "block",
-        javaSourceLink: "https://github.com/L1fe0nMars/Blackjack",
     },
 ];
 
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ProjectPortfolio
 
-A portfolio of various website and web application projects developed from scratch by myself. These projects were pure HTML, CSS, and JavaScript.
+A portfolio of various website projects developed from scratch by myself.
 
+- **[WeatherGuesser:](https://master--keen-torte-d6dd9d.netlify.app/)** A weather guessing game built with React utilizing the [OpenWeatherMap API](https://openweathermap.org/). Users are given a random city from anywhere in the world and some weather info and have to guess the current temperature in that city.
 - **[21 Classics:](https://21classics.com/)** A collector car website with a simple, elegant, and responsive design.
 - **[Blackjack:](https://l1fe0nmars.github.io/Blackjack/index.html)** Play Blackjack, the classic casino card game.
 - **[Planet Name Generator:](https://l1fe0nmars.github.io/PlanetNameGenerator/index.html)** Generate billions of unique sci-fi planet names.
