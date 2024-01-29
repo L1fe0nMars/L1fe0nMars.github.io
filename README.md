dd4129e74240f0977bc7d9f718158cfc61d4a25d
L1fe0nMars <jkama005@gmail.com>
2024-01-29 01:21:04 -0500

    Finished password generator project

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ProjectPortfolio
 
-A portfolio of various website projects developed from scratch by myself.
+A portfolio of various front-end projects developed from scratch by myself.
 
 - **[WeatherGuesser:](https://master--keen-torte-d6dd9d.netlify.app/)** A weather guessing game built with React utilizing the [OpenWeatherMap API](https://openweathermap.org/). Users are given a random city from anywhere in the world and some weather info and have to guess the current temperature in that city.
 - **[21 Classics:](https://21classics.com/)** A collector car website with a simple, elegant, and responsive design.
 - **[Blackjack:](https://l1fe0nmars.github.io/blackjack/)** Play Blackjack, the classic casino card game.
+- **[Password Generator:](https://l1fe0nmars.github.io/password-generator/)** A simple, secure password generator with different customization options.
 - **[Planet Name Generator:](https://l1fe0nmars.github.io/planet-name-generator/)** Generate billions of unique sci-fi planet names.
--- password-generator/src/password-generator.js
+++ password-generator/src/password-generator.js
@@ -1,26 +1,82 @@
 "use strict";
 
+const COPY_ICON = document.getElementById('copy-icon');
 const PASSWORD_CONTAINER = document.getElementById('password-container');
 const PASSWORD = document.getElementById('password')
 const PASSWORD_BTN = document.getElementById('password-btn');
 const PASSWORD_LENGTH = document.getElementById('password-length');
-const UPPERCASE = document.getElementById('uppercase');
-const LOWERCASE = document.getElementById('lowercase');
-const NUMBERS = document.getElementById('numbers');
-const SYMBOLS = document.getElementById('symbols');
+const UPPERCASE_CHECKBOX = document.getElementById('uppercase');
+const LOWERCASE_CHECKBOX = document.getElementById('lowercase');
+const NUMBERS_CHECKBOX = document.getElementById('numbers');
+const SYMBOLS_CHECKBOX = document.getElementById('symbols');
 
-const MIN_CHARS = 6;
+const MIN_CHARS = 8;
 const MAX_CHARS = 24;
+
+const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+const NUMBERS = '0123456789';
+const SYMBOLS = "`~!@#$%^&*()-_=+[]{}\|;:',.<>/?";
+
+/**
+ * Checks if at least one checkbox is checked
+ * 
+ * @return {boolean} Whether any box is checked or not
+ */
+function anyBoxIsChecked() {
+    return UPPERCASE_CHECKBOX.checked 
+        || LOWERCASE_CHECKBOX.checked
+        || NUMBERS_CHECKBOX.checked
+        || SYMBOLS_CHECKBOX.checked;
+}
 
 /**
  * Generates a random password
  * 
- * @param {string} length The length of the password
+ * @param {number} length The length of the password
+ * 
+ * @return {string} The generated password
  */
 function generatePassword(length) {
+    let chars = '';
+    let password = '';
+    const arr = new Uint32Array(length);
+    window.crypto.getRandomValues(arr);
     
+    if (!anyBoxIsChecked() || LOWERCASE_CHECKBOX.checked) {
+        chars += LOWERCASE;
+        LOWERCASE_CHECKBOX.checked = true;
+    }
+    if (UPPERCASE_CHECKBOX.checked) {
+        chars += UPPERCASE;
+    }
+    if (NUMBERS_CHECKBOX.checked) {
+        chars += NUMBERS;
+    }
+    if (SYMBOLS_CHECKBOX.checked) {
+        chars += SYMBOLS;
+    }
+    
+    for (let i = 0; i < length; i++) {
+        password += chars.charAt(arr[i] % chars.length);
+    }
+    
+    return password;
 }
 
+PASSWORD_CONTAINER.addEventListener('click', () => {
+    navigator.clipboard.writeText(PASSWORD.innerText);
+
+    COPY_ICON.classList.toggle('fa-copy');
+    COPY_ICON.classList.toggle('fa-circle-check');
+    COPY_ICON.style.color = "green";
+
+    setTimeout(() => {
+        COPY_ICON.classList.toggle('fa-copy');
+        COPY_ICON.classList.toggle('fa-circle-check');
+        COPY_ICON.style.color = "var(--btn)";
+    }, 2000);
+});
 
 PASSWORD_BTN.addEventListener('click', () => {
     let length = Number(PASSWORD_LENGTH.value);
@@ -34,8 +90,5 @@ PASSWORD_BTN.addEventListener('click', () => {
         PASSWORD_LENGTH.value = MAX_CHARS;
     }
 
-    
-
-    console.log(length);
-    console.log(typeof(length));
+    PASSWORD.innerText = generatePassword(length);
 });
